82a659636f0c2620aa3e44969dea3935701befcd
anne <annelecdoukayou@uni-oldenburg.de>
2020-08-31 18:53:17 +0200

    login.php

---
+++ login.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+    <link href="https://fonts.googleapis.com/css?family=Alef&display=swap" rel="stylesheet">
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="style.css"/>
+  </head>
+
+  <body>
+       <div class ="container"> 
+           <div id="header"> 
+              <h3> GÄSTE-BUCH </h3>
+            </div>
+            <div id="innercontainer">  
+                <div id="header2"> 
+                 <h3> LOGIN </h3>
+                </div>
+                <form action="/action_page.php">
+    <div class="form-group">
+      <label for="email">Email:</label>
+      <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
+    </div>
+    <div class="form-group">
+      <label for="pwd">Password:</label>
+      <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pwd">
+    </div>
+                </form>
+                <div id="footer"> 
+                  <h3 id="h3"> Anmelden </h3>
+                </div>
+           </div>
+
+    <div id="innercontainer2" >
+
+    <img src="https://img.icons8.com/ios/50/000000/add-user-male.png"> 
+         &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp   
+    neu ? hier registrieren <img>
+    </img>  
+    </div>
+  </div>
+  </body>
+</html>
